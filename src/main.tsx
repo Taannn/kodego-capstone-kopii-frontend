c5f65812fcc6024c94a9d4a2f32d51f203e3211c
Tristan Candelario <tristan.candelario06@gmail.com>
2024-03-13 11:34:03 +0800

    trying to fix broken carousel

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux'
 import store from './app/store.ts'
 import './assets/css/styles.css'
 import './assets/css/hover.css'
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
